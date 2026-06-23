daa135956f9c62397950e6578eb0a317b2e0cc49
Master <master@192.168.1.27>
2026-06-23 17:22:01 -0400

    nueva configuracion de exepciones

--- src/main/java/cl/dgac/huinspector/dtos/DtoExepciones.java
+++ src/main/java/cl/dgac/huinspector/dtos/DtoExepciones.java
@@ -1,12 +1,16 @@
 package cl.dgac.huinspector.dtos;
 
 import java.time.LocalDateTime;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record DtoExepciones(
-    LocalDateTime timestap,
-    Integer codigoestado,
+    LocalDateTime fecha,
+    int codigoHttp,
     String error,
     String mensaje,
+    Map<String, String> detalles,
     String ruta
 ) {
 
--- src/main/java/cl/dgac/huinspector/exepcion/ManejoExepcionesGlobales.java
+++ src/main/java/cl/dgac/huinspector/exepcion/ManejoExepcionesGlobales.java
@@ -1,47 +1,45 @@
 package cl.dgac.huinspector.exepcion;
 
 import java.time.LocalDateTime;
-
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.reactive.function.client.WebClientRequestException;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
-
 import cl.dgac.huinspector.dtos.DtoExepciones;
+import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ManejoExepcionesGlobales {
-    private ResponseEntity<DtoExepciones> construirRespuesta(HttpStatus status, String mensaje, WebRequest request) {
-        String ruta = request.getDescription(false).replace("uri=", "");
-        
+    private ResponseEntity<DtoExepciones> buildResponse(
+        HttpStatus status,String mnesaje,
+        String ruta,Map<String, String> detalles){
         DtoExepciones dto = new DtoExepciones(
-            LocalDateTime.now(),
-            status.value(),
-            status.getReasonPhrase(),
-            mensaje,
-            ruta
-        );
-        
-        return ResponseEntity.status(status).body(dto);
-    }
-
+            LocalDateTime.now(),status.value(),
+            status.getReasonPhrase(),mnesaje,
+            detalles,ruta);
+            return ResponseEntity.status(status).body(dto);
+        };
     
-
     @ExceptionHandler(WebClientRequestException.class)
-    public ResponseEntity<DtoExepciones> manejarServicioCaido(WebClientRequestException ex, WebRequest request) {
-        
-        return construirRespuesta(HttpStatus.BAD_GATEWAY, "microservicio caido", request);
+    public ResponseEntity<DtoExepciones> manejarServicioCaido(WebClientRequestException ex) {
+        return buildResponse(HttpStatus.BAD_GATEWAY, "microservicio externo no responde ",ex.getUri().toString(),null);
     }
     @ExceptionHandler(WebClientResponseException.class)
-    public ResponseEntity<String> manejarErrorWebClient(WebClientResponseException ex) {
-        String jsonOriginal = ex.getResponseBodyAsString();
-        return ResponseEntity
-                .status(ex.getStatusCode())
-                .header("Content-Type", "application/json")
+    public ResponseEntity<DtoExepciones> manejarErrorWebClient(WebClientResponseException ex) {
+        DtoExepciones jsonOriginal = ex.getResponseBodyAs(DtoExepciones.class);
+        return ResponseEntity.status(ex.getStatusCode())
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .body(jsonOriginal);
+    }
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<DtoExepciones> handleRuntime(RuntimeException ex,HttpServletRequest request) {
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR,
+            "Error Interno del Servidor", request.getRequestURI(), null);
     }
 
 }
